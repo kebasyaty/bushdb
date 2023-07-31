a04b381e2228ac7d60f5cfaf6b454cb9d048170a
Gennady Kostyunin <kebasyaty@gmail.com>
2023-07-31 11:32:26 +0300

    Add napalm() method

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,7 +1,11 @@
 require "spec"
+require "file_utils"
+
 require "../src/bushdb"
 
-def create_test_store : BushDB::Store
-  store = BushDB::Store.new
-  store
+def create_test_db : BushDB::DB
+  db : BushDB::DB = BushDB::DB.new
+  db.root_store = "BushDB_crystal_test"
+  db.db_name = "store_crystal_test"
+  db
 end
