1f31c85246f0048f96f3f5dee3eea343fcdaf027
Gennady Kostyunin <kebasyaty@gmail.com>
2023-07-31 16:16:51 +0300

    Update class ErrorKeyMissing

--- src/bushdb/bush_errors.cr
+++ src/bushdb/bush_errors.cr
@@ -1,8 +1,5 @@
 # Custom exceptions for BushDB.
 module BushDB
-  property message : String | Nil
-  property cause : Exception | Nil
-
   class ErrorKeyMissing < Exception
     def initialize(key : String, @cause : Exception | Nil = nil)
       @message = %(The "#{key}" key is missing.)
