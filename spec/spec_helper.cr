2bab83fb1b34a03d9cf4e57c083e1b42e6bc5be8
Gennady Kostyunin <kebasyaty@gmail.com>
2023-08-02 09:01:11 +0300

    Change the type for the root_store property

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -5,7 +5,7 @@ require "../src/bushdb"
 
 def create_test_db : BushDB::DB
   db : BushDB::DB = BushDB::DB.new
-  db.root_store = "BushDB_crystal_test"
+  db.root_store = Path.new("BushDB_crystal_test")
   db.db_name = "store_crystal_test"
   db
 end
