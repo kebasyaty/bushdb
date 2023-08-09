0408f3535b0116186957d711c2fbb2077a477b9c
Gennady Kostyunin <kebasyaty@gmail.com>
2023-08-09 20:58:09 +0300

    Update tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -13,14 +13,12 @@ end
 # Testig the creation of an instance.
 describe ".new" do
   it "create an instance of the DB" do
-    db = BushDB::DB.new
-    db.should_not be_nil
+    db : BushDB::DB = BushDB::DB.new
     db.root_store.should eq Path.new("BushDB")
     db.db_name.should eq "store"
   end
   it "create_test_db method" do
     db : BushDB::DB = create_test_db
-    db.should_not be_nil
     db.root_store.should eq Path.new("BushDB_crystal_test")
     db.db_name.should eq "store_crystal_test"
   end
